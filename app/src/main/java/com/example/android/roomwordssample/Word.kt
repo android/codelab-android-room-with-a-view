4f2ec3afcca4bb9781a654109d898b39755313a0
Florina Muntenescu <florinam@google.com>
2018-09-25 14:07:54 +0100

    Converting the code to Kotlin

--- app/src/main/java/com/example/android/roomwordssample/Word.kt
+++ app/src/main/java/com/example/android/roomwordssample/Word.kt
@@ -1,4 +1,4 @@
-package com.example.android.roomwordssample;
+package com.example.android.roomwordssample
 
 /*
  * Copyright (C) 2017 Google Inc.
@@ -16,10 +16,9 @@ package com.example.android.roomwordssample;
  * limitations under the License.
  */
 
-import android.arch.persistence.room.ColumnInfo;
-import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.PrimaryKey;
-import android.support.annotation.NonNull;
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
 
 /**
  * A basic class representing an entity that is a row in a one-column database table.
@@ -33,19 +32,4 @@ import android.support.annotation.NonNull;
  */
 
 @Entity(tableName = "word_table")
-public class Word {
-
-    @PrimaryKey
-    @NonNull
-    @ColumnInfo(name = "word")
-    private String mWord;
-
-    public Word(@NonNull String word) {
-        this.mWord = word;
-    }
-
-    @NonNull
-    public String getWord() {
-        return this.mWord;
-    }
-}
+data class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
--- app/src/main/java/com/example/android/roomwordssample/WordDao.kt
+++ app/src/main/java/com/example/android/roomwordssample/WordDao.kt
@@ -1,4 +1,4 @@
-package com.example.android.roomwordssample;
+package com.example.android.roomwordssample
 
 /*
  * Copyright (C) 2017 Google Inc.
@@ -16,12 +16,10 @@ package com.example.android.roomwordssample;
  * limitations under the License.
  */
 
-import android.arch.lifecycle.LiveData;
-import android.arch.persistence.room.Dao;
-import android.arch.persistence.room.Insert;
-import android.arch.persistence.room.Query;
-
-import java.util.List;
+import android.arch.lifecycle.LiveData
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
 
 /**
  * The Room Magic is in this file, where you map a Java method call to an SQL query.
@@ -33,21 +31,21 @@ import java.util.List;
  */
 
 @Dao
-public interface WordDao {
+interface WordDao {
 
     // LiveData is a data holder class that can be observed within a given lifecycle.
     // Always holds/caches latest version of data. Notifies its active observers when the
     // data has changed. Since we are getting all the contents of the database,
     // we are notified whenever any of the database contents have changed.
     @Query("SELECT * from word_table ORDER BY word ASC")
-    LiveData<List<Word>> getAlphabetizedWords();
+    fun getAlphabetizedWords(): LiveData<List<Word>>
 
     // We do not need a conflict strategy, because the word is our primary key, and you cannot
     // add two items with the same primary key to the database. If the table has more than one
     // column, you can use @Insert(onConflict = OnConflictStrategy.REPLACE) to update a row.
     @Insert
-    void insert(Word word);
+    fun insert(word: Word)
 
     @Query("DELETE FROM word_table")
-    void deleteAll();
+    fun deleteAll()
 }
