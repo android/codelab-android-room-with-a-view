7baf34de76214f929eae37d272380f6f4dae16ab
Florina Muntenescu <florinam@google.com>
2018-09-25 14:07:54 +0100

    Converting the code to Kotlin

---
+++ app/src/main/java/com/example/android/roomwordssample/WordListAdapter.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.roomwordssample
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class WordListAdapter internal constructor(
+        context: Context
+) : RecyclerView.Adapter<WordListAdapter.WordViewHolder>() {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+    private var words = emptyList<Word>() // Cached copy of words
+
+    inner class WordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val wordItemView: TextView = itemView.findViewById(R.id.textView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        val itemView = inflater.inflate(R.layout.recyclerview_item, parent, false)
+        return WordViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        val current = words[position]
+        holder.wordItemView.text = current.word
+    }
+
+    internal fun setWords(words: List<Word>) {
+        this.words = words
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount() = words.size
+}
+
+
---
+++ app/src/main/java/com/example/android/roomwordssample/WordRoomDatabase.kt
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.roomwordssample
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+/**
+ * This is the backend. The database. This used to be done by the OpenHelper.
+ * The fact that this has very few comments emphasizes its coolness.
+ */
+@Database(entities = [Word::class], version = 1)
+abstract class WordRoomDatabase : RoomDatabase() {
+
+    abstract fun wordDao(): WordDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(
+                context: Context,
+                scope: CoroutineScope
+        ): WordRoomDatabase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        WordRoomDatabase::class.java,
+                        "word_database"
+                )
+                        // Wipes and rebuilds instead of migrating if no Migration object.
+                        // Migration is not part of this codelab.
+                        .fallbackToDestructiveMigration()
+                        .addCallback(WordDatabaseCallback(scope))
+                        .build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+
+        private class WordDatabaseCallback(
+                private val scope: CoroutineScope
+        ) : RoomDatabase.Callback() {
+            /**
+             * Override the onOpen method to populate the database.
+             * For this sample, we clear the database every time it is created or opened.
+             */
+            override fun onOpen(db: SupportSQLiteDatabase) {
+                super.onOpen(db)
+                // If you want to keep the data through app restarts,
+                // comment out the following line.
+                INSTANCE?.let { database ->
+                    scope.launch(Dispatchers.IO) {
+                        populateDatabase(database.wordDao())
+                    }
+                }
+            }
+        }
+
+        /**
+         * Populate the database in a new coroutine.
+         * If you want to start with more words, just add them.
+         */
+        fun populateDatabase(wordDao: WordDao) {
+            // Start the app with a clean database every time.
+            // Not needed if you only populate on creation.
+            wordDao.deleteAll()
+
+            var word = Word("Hello")
+            wordDao.insert(word)
+            word = Word("World!")
+            wordDao.insert(word)
+        }
+    }
+
+}
