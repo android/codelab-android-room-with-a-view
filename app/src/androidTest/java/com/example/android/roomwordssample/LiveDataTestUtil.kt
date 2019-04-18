7c321a9dcf526452099e924a120ac12dba1bb87d
Florina Muntenescu <florinam@google.com>
2019-04-18 17:45:35 +0100

    Jetifying the Kotlin branch

--- app/src/androidTest/java/com/example/android/roomwordssample/LiveDataTestUtil.kt
+++ app/src/androidTest/java/com/example/android/roomwordssample/LiveDataTestUtil.kt
@@ -16,8 +16,8 @@ package com.example.android.roomwordssample
  * limitations under the License.
  */
 
-import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.Observer
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
