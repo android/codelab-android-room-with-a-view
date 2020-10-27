7487efc9797f82ac064b1633a95f5e1b3b95db9d
Florina Muntenescu <florinam@google.com>
2020-10-27 19:35:21 +0000

    Improving the app architecture: * The database now uses an app level scope, rather than being tied to the viewmodel scope * The viewmodel now receives its dependencies as parameter, rather than creating it * The viewmodel is created using a factory * The viewmodel is instantiated using the by viewModels delegate

--- app/src/main/java/com/example/android/roomwordssample/MainActivity.kt
+++ app/src/main/java/com/example/android/roomwordssample/MainActivity.kt
@@ -20,9 +20,9 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -30,7 +30,9 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 class MainActivity : AppCompatActivity() {
 
     private val newWordActivityRequestCode = 1
-    private lateinit var wordViewModel: WordViewModel
+    private val wordViewModel: WordViewModel by viewModels {
+        WordViewModelFactory((application as WordsApplication).repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,9 +42,6 @@ class MainActivity : AppCompatActivity() {
         val adapter = WordListAdapter(this)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
-
-        // Get a new or existing ViewModel from the ViewModelProvider.
-        wordViewModel = ViewModelProvider(this).get(WordViewModel::class.java)
 
         // Add an observer on the LiveData returned by getAlphabetizedWords.
         // The onChanged() method fires when the observed data changes and the activity is
---
+++ app/src/main/java/com/example/android/roomwordssample/WordsApplication.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.roomwordssample
+
+import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+
+class WordsApplication : Application() {
+    // No need to cancel this scope as it'll be torn down with the process
+    val applicationScope = CoroutineScope(SupervisorJob())
+
+    // Using by lazy so the database and the repository are only created when they're needed
+    // rather than when the application starts
+    val database by lazy { WordRoomDatabase.getDatabase(this, applicationScope) }
+    val repository by lazy { WordRepository(database.wordDao()) }
+}
